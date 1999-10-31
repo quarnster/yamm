d646a07e0b2b36a60aa9d6ad0b6a1398b1ec0930
Fredrik Ehnbom <quarnster@gmail.com>
1999-10-31 19:15:48 +0000

    The encoding bit moved out to util/UUEncoder.java

--- encode/UUEncode.java
+++ encode/UUEncode.java
@@ -23,102 +23,80 @@ import java.util.Vector;
 import java.util.StringTokenizer;
 import java.net.URL;
 import java.net.URLConnection;
+import org.gjt.fredde.util.UUEncoder;
 import org.gjt.fredde.yamm.YAMM;
 
 /**
  * Encodes file in the uuencode format
  */
 public class UUEncode {
-  byte[] ctmp =  new byte[45];
-  byte[] outputBuffer = new byte[4];
-  int    count = 0;
-  int    i = 0;
-  int    linecnt = 0;
-  String filename;
-  DataOutputStream out;
+	protected String filename;
+	protected DataOutputStream out;
 
-  /**
-   * Encodes all files in the specified vector
-   * @param attach The files to encode
-   */
-  public UUEncode(Vector attach) {
-    try {
-      out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(System.getProperty("user.home") + "/.yamm/boxes/" + YAMM.getString("box.outbox"), true)));
+	/**
+	 * Encodes all files in the specified vector
+	 * @param attach The files to encode
+	 */
+	public UUEncode(Vector attach) {
+		try {
+			out = new DataOutputStream(new BufferedOutputStream(
+				new FileOutputStream(YAMM.home + "/boxes/" +
+					YAMM.getString("box.outbox"), true)));
 
-      out.writeBytes("--AttachThis");
-      for(int i = 0; i < attach.size();i++) {
-        StringTokenizer tok = new StringTokenizer(attach.elementAt(i).toString(), System.getProperty("file.separator"));
+			out.writeBytes("--AttachThis");
+			for (int i = 0; i < attach.size(); i++) {
+				StringTokenizer tok = new StringTokenizer(
+						attach.elementAt(i).toString(),
+								YAMM.sep);
 
-        URL url = new URL("file:///" + attach.elementAt(i).toString());
-        URLConnection uc = url.openConnection();
-        uc.connect(); 
-        String ctype = uc.getContentType();
+				URL url = new URL("file:///" +
+						attach.elementAt(i).toString());
+				URLConnection uc = url.openConnection();
+				uc.connect(); 
+				String ctype = uc.getContentType();
 
-        if(!(tok.countTokens() <= 1))
-          for(;1 < tok.countTokens();tok.nextToken()) {}
-        filename = tok.nextToken();
+				if (!(tok.countTokens() <= 1)) {
+					for (; 1 < tok.countTokens();
+							tok.nextToken());
+				}
+				filename = tok.nextToken();
 
         
-        out.writeBytes("\nContent-Type: " + ctype + "; name=\"" + filename + "\"\nContent-Transfer-Encoding: x-uuencode\nContent-Disposition: attachment; filename=\"" + filename + "\"\n\n");
-        encodeFile(attach.elementAt(i).toString());
+				out.writeBytes("\nContent-Type: " + ctype +
+						"; name=\"" + filename +
+					"\"\nContent-Transfer-Encoding: " +
+							" x-uuencode" +
+					"\nContent-Disposition: attachment; " +
+					"filename=\"" + filename + "\"\n\n");
+				encodeFile(attach.elementAt(i).toString());
 
-        if(i+1 < attach.size()) out.writeBytes("\n--AttachThis");
-        else out.writeBytes("\n--AttachThis--");
-      }
-      out.writeBytes("\n.\n\n");
-      out.close();
+				if (i + 1 < attach.size()) {
+					out.writeBytes("\n--AttachThis");
+				} else {
+					out.writeBytes("\n--AttachThis--");
+				}
+			}
+			out.writeBytes("\n.\n\n");
+			out.close();
+		} catch(IOException ioe) {
+			System.err.println(ioe);
+		}
+	}
 
-    } catch(IOException ioe) { System.err.println(ioe); }
-  }
+	/**
+	 * Encodes the specified file
+	 * @param file The file to encode
+	 */
+	protected void encodeFile(String file) {
+		try {
+			DataInputStream in = new DataInputStream(
+						new FileInputStream(file));
 
-  /**
-   * Encodes the specified file
-   * @param file The file to encode
-   */
-  protected void encodeFile(String file) {
-    try {
-      DataInputStream in = new DataInputStream(new FileInputStream(file));
-
-      out.writeBytes("begin 644 " + filename + "\n");
-      out.flush();
-
-      for(;;) {
-        ctmp = new byte[45];
-        if(in.read(ctmp, 0, 45) == -1) break;
-
-        int i;
-        out.write(encodeByte((byte)ctmp.length));
-        out.flush();
-
-        for(linecnt = ctmp.length-1, i = 0;linecnt > 0;linecnt -=3,i +=3) {
-          outputBuffer[0] = encodeByte((ctmp [i] & 0xFC) >> 2);
-          outputBuffer[1] = encodeByte(((ctmp[i] & 0x03) << 4) | ((ctmp[i + 1] & 0xF0) >> 4));
-          outputBuffer[2] = encodeByte(((ctmp[i + 1] & 0x0F) << 2) | ((ctmp[i + 2] & 0xC0) >> 6));
-          outputBuffer[3] = encodeByte(ctmp[i + 2] & 0x3F);
-
-          out.write(outputBuffer);
-          out.flush();
-        }
-
-        out.writeBytes("\n");
-        out.flush();
-      } 
-
-      out.writeBytes("`\nend\n");
-      out.flush();
-
-      in.close();
-    }
-    catch (IOException ioe) {
-      System.err.println(ioe);
-    }
-  }
-
-  /**
-   * Encodes an int to an uuencoded byte
-   * @param b The integer to convert
-   */
-  protected byte encodeByte(int b) {
-    return (byte)(((b) == 0) ? ((byte)96) : ((b & 077) + 32));
-  }
+			UUEncoder uuenc = new UUEncoder(filename);
+			uuenc.encode(in, out);
+			in.close();
+		} catch (IOException ioe) {
+			System.err.println(ioe);
+		}
+	}
 }
