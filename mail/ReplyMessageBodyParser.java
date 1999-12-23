1b2ba808fc70273cdb9f4c3aa6aca432b757ca3e
Fredrik Ehnbom <quarnster@gmail.com>
1999-12-23 14:09:50 +0000

    Takes care of preparing a message for reply

---
+++ mail/ReplyMessageBodyParser.java
@@ -0,0 +1,146 @@
+/*  MessageBodyParser.java - Parses the body of a message
+ *  Copyright (C) 1999 Fredrik Ehnbom
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.gjt.fredde.yamm.mail;
+
+import java.text.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.JTextArea;
+
+/**
+ * Parses the body of a message
+ */
+public class ReplyMessageBodyParser {
+
+	public static final int END = 0;
+	public static final int ERROR = 1;
+	public static final int ATTACHMENT = 2;
+
+	protected boolean html = false;
+
+	public static String unMime(String m, boolean html) {
+		char[] check = "0123456789ABCDEF".toCharArray();
+		StringTokenizer tok = new StringTokenizer(m);
+		String mime2 = "";
+
+		while (tok.hasMoreTokens()) {
+			String mime = tok.nextToken();
+
+			if (mime.indexOf("=") != -1) {
+
+				if (mime.toLowerCase().indexOf("://") != -1) {
+					mime2 += mime + " ";
+					continue;
+				}
+
+				for (int index = mime.indexOf("=") + 1;;
+					index = mime.indexOf("=", index) + 1) {
+
+					if (index == 0) {
+						break;
+					} else if (index + 2 > mime.length()) {
+						break;
+					}
+					char[] hex = mime.substring(index,
+						index + 2).toCharArray();
+
+					boolean char1 = false;
+					boolean char2 = false;
+
+					for (int i = 0; i < 16; i++) {
+						if (hex[0] == check[i]) {
+							char1 = true;
+						}
+						if (hex[1] == check[i]) {
+							char2 = true;
+						}
+					}
+
+					if (char1 && char2) {
+						int asciiChar =
+							Integer.parseInt(
+							new String(hex), 16);
+						String begin = mime.substring(0,
+								index - 1);
+						String end = mime.substring(
+								index + 2,
+								mime.length());
+						if (html) {
+							mime = begin + "&#" +
+								asciiChar +
+								";" + end;
+						} else {
+							mime = begin +
+								(char) asciiChar
+								+ end;
+						}
+					}
+				}
+				mime2 += mime + " ";
+			} else {
+				mime2 += mime + " ";
+			}
+		}
+
+		return mime2;
+	}
+
+	public ReplyMessageBodyParser(boolean html) {
+		this.html = html;
+	}
+
+	public int parse(BufferedReader in, JTextArea out,
+				String attachment) throws IOException,
+							MessageParseException {
+		String temp = in.readLine();
+
+		for (;;) {
+
+			if (temp == null) {
+				throw new MessageParseException("Unexpected" +
+							"end of message.");
+			}
+
+			if (temp.equals(".")) {
+				return END;
+			}
+
+			if (temp.startsWith("--" + attachment)) {
+				if (temp.endsWith("-- ")) {
+					return END;
+				} else {
+					return ATTACHMENT;
+				}
+			}
+
+			if (temp.endsWith("=") && !temp.endsWith("==")) {
+				temp = temp.substring(0, temp.length() - 1);
+				temp += in.readLine();
+				continue;
+			}
+
+			if (temp.indexOf("=") != -1) {
+				temp = unMime(temp, html);
+			}
+
+			out.append(">" + temp + "\n");
+			temp = in.readLine();
+		}
+	}
+}
