686a8f9a7169a70bfb2c3c0cc7113918d2337b07
Fredrik Ehnbom <quarnster@gmail.com>
2000-03-05 17:57:17 +0000

    cleaned up and now uses urls instead of strings for the image

--- gui/SplashScreen.java
+++ gui/SplashScreen.java
@@ -1,5 +1,5 @@
 /*  SplashScreen.java - a splashscreen
- *  Copyright (C) 1999 Fredrik Ehnbom
+ *  Copyright (C) 1999, 2000 Fredrik Ehnbom
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -26,23 +26,32 @@ import java.net.URL;
 
 /**
  * A splashscreen to display while YAMM loads
+ * @author Fredrik Ehnbom
+ * @version $Id: SplashScreen.java,v 1.2 2000/03/05 17:57:17 fredde Exp $
  */
 public class SplashScreen extends JWindow {
 
-  /** Creates and show the splashscreen */
-  public SplashScreen(String text, String pic) {
-    getContentPane().setLayout(new BorderLayout());
-    getContentPane().setFont(new Font("Monospaced",0,14));
+	/** Creates and show the splashscreen */
+	public SplashScreen(String text, URL pic) {
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().setFont(new Font("Monospaced",0,14));
 
-    JLabel logo = new JLabel(new ImageIcon(pic));
-    Border ram = BorderFactory.createBevelBorder(BevelBorder.RAISED);
-    logo.setBorder(ram);
+		JLabel logo = new JLabel(new ImageIcon(pic));
+		Border ram = BorderFactory.createBevelBorder(BevelBorder.RAISED);
+		logo.setBorder(ram);
 
-    getContentPane().add("Center", logo);
-    getContentPane().add("South",new JLabel(text, SwingConstants.CENTER));
-    Dimension screen = getToolkit().getScreenSize();
-    pack();
-    setLocation((screen.width - getSize().width) / 2, (screen.height - getSize().height) / 2);
-    show();
-  }
+		getContentPane().add("Center", logo);
+		getContentPane().add("South",new JLabel(text, SwingConstants.CENTER));
+		Dimension screen = getToolkit().getScreenSize();
+		pack();
+		setLocation((screen.width - getSize().width) / 2, (screen.height - getSize().height) / 2);
+		show();
+	}
 }
+/*
+ * Changes:
+ * $Log: SplashScreen.java,v $
+ * Revision 1.2  2000/03/05 17:57:17  fredde
+ * cleaned up and now uses urls instead of strings for the image
+ *
+ */
